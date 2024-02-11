1553884c86db5d255de269e83a3fcd64b701fa15
anil.senocak <anil.senocak@turkcell.com.tr>
2024-02-11 23:09:34 +0300

    refactoring

--- react-ts/src/store/reducers.ts
+++ react-ts/src/store/reducers.ts
@@ -1,13 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import meSlice from "./features/auth/meSlice"
-import getSinglePostSlice from "./features/post/getSinglePostSlice"
-import addCommentSlice from "./features/post/addCommentSlice"
 import loginSlice from "./features/auth/loginSlice"
-import createPostSlice from "./features/post/createPostSlice"
-import getAllPostsSlice from "./features/post/getAllPostsSlice"
-import deletePostSlice from "./features/post/deletePostSlice"
-import patchPostSlice from "./features/post/patchPostSlice"
-import patchCommentVisibilitySlice from "./features/post/patchCommentVisibilitySlice"
 import registerSlice from "./features/auth/registerSlice"
 import activateSlice from "./features/auth/activateSlice"
 import resendEmailActivationSlice from "./features/auth/resendEmailActivationSlice"
@@ -23,15 +16,5 @@ export default combineReducers({
     resendEmailActivation: resendEmailActivationSlice,
     logout: logoutSlice,
     resetPassword: resetPasswordSlice,
-    changePassword: changePasswordSlice,
-
-    addComment: addCommentSlice,
-
-    getAllPosts: getAllPostsSlice,
-    getSinglePost: getSinglePostSlice,
-    createPost: createPostSlice,
-    deletePost: deletePostSlice,
-    patchPost: patchPostSlice,
-    patchCommentVisibility: patchCommentVisibilitySlice,
-
+    changePassword: changePasswordSlice
 })
